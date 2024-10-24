eaf86c1a8dae1381547031c63127bb29e35aa642
eyob worku <eyobworku62@gmail.com>
2024-10-24 18:43:24 +0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Example from "./Example";
 import TableCollapse from "./components/TableCollapse";
 
 export default function App() {
